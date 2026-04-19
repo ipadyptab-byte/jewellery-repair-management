701fb7faeabfdf1f4c577423a0dc0d383ac2771c
ipadyptab-byte <ipadyptab@gmail.com>
2026-04-19 18:10:52 +0530

    Update route.ts

--- src/app/api/send-whatsapp/route.ts
+++ src/app/api/send-whatsapp/route.ts
@@ -59,7 +59,7 @@ export async function POST(req: NextRequest) {
         template_name: 'delivery_otp_dj_3',
         lang_code: 'en',
                 body: [
-                    { text: otp || '0000' }
+                    { text: COPY CODE || '0000' }
         ]
       }
     };
