4ffb3be3d61994e7ec9be3730fec16fff02d6e2c
ipadyptab-byte <ipadyptab@gmail.com>
2026-04-20 11:50:46 +0530

    Update route.ts

--- src/app/api/send-whatsapp/route.ts
+++ src/app/api/send-whatsapp/route.ts
@@ -62,7 +62,7 @@ if (!phoneClean.startsWith('91')) {
 const payload = {
       phone: phone,
       media: {
-        type: 'template',
+        type: 'media_template',
         template_name: 'delivery_otp_dj_3',
         lang_code: 'en',
         body: [
